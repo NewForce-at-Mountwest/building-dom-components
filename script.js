c2221791ce4c9d15979593c823bb86399a2d9e1a
Jordan Castelloe <jordan.castelloe@gmail.com>
2019-01-06 13:09:48 -0500

    added demo code for building html components

---
+++ script.js
@@ -0,0 +1,13 @@
+// What if we want to build the same HTML components multiple times? Example: print the same info for every student in the class 
+
+// Let's write a function to build the following HTML:
+//<h1 class="xx-large">Jordan Castelloe</h1>
+
+// It should take two arguments of class and element
+
+// Now call the function and set the innerHTML of #container to the returned HTML component
+
+//LIGHTNING EXERCISE ONE
+
+// Now let's build a function called buildStudentComponent that calls both our h1 function and our function from the lightning exercise
+
