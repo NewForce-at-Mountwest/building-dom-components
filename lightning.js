df4c416e93ce56d703dac8ebc904b1f8fe4c0400
Jordan Castelloe <jordan.castelloe@gmail.com>
2019-01-23 18:51:02 -0500

    fleshed out demo code

---
+++ lightning.js
@@ -0,0 +1,39 @@
+//1.
+// - Create three new files: an `index.html`, a JavaScript file, and a CSS file
+// - Copy and paste the following code into your `index.html` file:
+// <article id="container"></article>
+// 
+// - Write a function called buildSection that accepts two arguments: `text` and `style`
+// - The function should return an HTML component of a section element with whatever text and class (`style`) you pass in. 
+// - Go ahead and add some CSS rules to whatever style you pass in so you can see if it worked or not.
+// - Call the function and insert your section into the DOM
+
+// 2.
+// - Copy and paste the following code into your javascript file
+
+const section = function(textParameter) {
+    return `<section class="bordered dashed">${textParameter}</section>`
+}
+
+const h3 = function(textParameter) {
+    return `<h3>${textParameter}</h3>`
+}
+
+const unorderedList = function(arrayParameter) {
+    let list = "";
+    for(let i = 0; i < arrayParameter.length; i++){
+        list += `<li>${arrayParameter[i]}</li>`
+    }
+    return `<ul>${list}</ul>`
+}
+
+// - Your job is to build a function called buildStudentComponent. The student component should include an h3 element, a section element, and a bulleted list of technologies that you've learned so far
+
+// - The buildStudentComponent function should call the three functions you just copied and pasted in your file
+
+// - Bear in mind that the unorderedList function is expecting an array as a parameter- make sure you're passing in an array! 
+
+// - The buildStudentComponent function should RETURN the full HTML string of all the elements put together
+
+// - For now, just log the returned HTML string to the console. (Since you're returning it, be sure to catch it in a variable- when you're done you should be able to console.log it OUTSIDE the function.)
+
--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-// What if we want to build the same HTML components multiple times? Example: print the same info for every student in the class 
+// --------------------Review printing to the DOM ---------------------//
+
+// document.querySelector("#container").innerHTML = "<h1>Jordan Castelloe</h1>";
+
+
+// const header = "<h1>Jordan Castelloe</h1>"
+// const image = `<img src="https://avatars0.githubusercontent.com/u/19379122?s=460&v=4" alt="picture of doge meme">`
+// const paragraph = "<p>Isn't it fantastic that you can change your mind and create all these happy things? You can't have light without dark. You can't know happiness unless you've known sorrow. If it's not what you want - stop and change it. Don't just keep going and expect it will get better.</p>"
+
+// use string interpolation to combine all this stuff into one long html string and put it in the DOM
+
+//------------ FUNCTIONS THAT RETURN DOM COMPONENTS ------------------//
+
+// What if we want to build the same HTML components multiple times? Example: print the same info for everyone in the class. The students have a slightly different component structure than the instructors, but they have the same basic elements. 
+
+// Rather than building the whole thing at once, our code will be more flexible if we break it down into smaller functions that build one HTML element at a time. Then we can combine them any way we want!
 
 // Let's write a function to build the following HTML:
-//<h1 class="xx-large">Jordan Castelloe</h1>
+//<h2 class="xx-large">Jordan Castelloe</h2>
 
-// It should take two arguments of class and element
+// Let's refactor it to take one parameter of the content we want to put between the HTML tags
+
+// AAAAnd let's refactor it again to accept a parameter of class- now we should be passing in two parameters
 
 // Now call the function and set the innerHTML of #container to the returned HTML component
 
 //LIGHTNING EXERCISE ONE
 
-// Now let's build a function called buildStudentComponent that calls both our h1 function and our function from the lightning exercise
+// And a function that returns an image component. It should accept parameters of src and alt
+
+// Now let's define a function called buildInstructorComponent that calls our h1 function, our image function, and our section function from the lightning exercise. Set the innerHTML of the container to be the WHOLE student component
+
+
+
 
